dd039d053f0fa8b334cbc2b002897450a9a9749a
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-02-21 17:13:51 -0800

    swapped background textures

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,14 +7,15 @@ jQuery(document).ready(function($){
 		e.preventDefault();
 	});
 	
-	$('#pitch .pitch-content').each(function(){
-		var imgSrc = $(this).find('img').attr('src');
+	if ($.browser.msie) {
+	//dont do anything for explorer
+	   }
+	   else{
+		var imgSrc = jQuery('#bottom').find('img').attr('src');
+		jQuery('#bottom').find('.texture').css({'width':'100%', 'height':'100%', 'background-image': 'url('+templateDir+'/images/paper_bg.png), url(' +imgSrc+')'});
+		jQuery('#bottom').find('img').css({'opacity':0});
+	}
 		
-		$(this).css({'background-image': 'url('+templateDir+'/images/paper_bg.png), url(' +imgSrc+')'});
-		$(this).find('img').css({'opacity':0});
-		console.log(templateDir);
-	});
-	
 	jQuery('.flexslider').flexslider({
 	  animation: "slide",
 	  selector: ".slides > div",
